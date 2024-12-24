7112dce07504c6d689d2fc8c98d30f61d687d2a1
SaribJawad <msssarib12@gmail.com>
2024-12-24 16:49:38 +0500

    Add .env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
+const API_BASE_URL = process.env.VITE_API_BASE_URL;
+
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "https://n5cm79p6wa.execute-api.ap-south-1.amazonaws.com/dev",
-        target: "http://localhost:3000/",
+        target: API_BASE_URL,
         changeOrigin: true,
         secure: true,
       },
