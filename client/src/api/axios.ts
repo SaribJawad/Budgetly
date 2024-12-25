0f630ab00410b346e6d0e92cec7c2527958d3992
SaribJawad <msssarib12@gmail.com>
2024-12-26 00:27:07 +0500

    test api base url

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -4,6 +4,8 @@ import { logout } from "@/features/auth/authSlice";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("API Base URL:", API_BASE_URL);
+
 export const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
