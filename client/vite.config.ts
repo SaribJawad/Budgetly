3768664217ecc199b50d1181a6018909b5b36c9c
SaribJawad <msssarib12@gmail.com>
2024-12-25 14:12:47 +0500

    Versioning untracked files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-const API_BASE_URL = process.env.VITE_API_BASE_URL;
+// const API_BASE_URL = process.env.VITE_API_BASE_URL;
 
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: API_BASE_URL,
+        target: "https://n5cm79p6wa.execute-api.ap-south-1.amazonaws.com/dev",
         changeOrigin: true,
         secure: true,
       },
