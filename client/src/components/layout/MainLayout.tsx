ef1e4fe4de8a36c8759caee477cf75bc0515b134
SaribJawad <msssarib12@gmail.com>
2025-03-24 13:41:34 +0500

    Remove unused import

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 import { AppSidebar } from "../navigation/AppSideBar";
 import { useAppSelector } from "@/app/hook";
